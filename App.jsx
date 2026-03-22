88e6fda9db5d5f74e47022b13f73d0411b3eb8df
Zacharia316 <zappyblues234@gmail.com>
2026-03-22 18:24:56 +0300

    Create initial App component for Sparks Dating App

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Sparks Dating App</h1>
+      <p>Welcome to Sparks by Lumira</p>
+    </div>
+  )
+}
+
+export default App
